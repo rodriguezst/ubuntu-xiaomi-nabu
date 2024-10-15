92b3ca183f9ded080e19b890c751c22c8a3f9e1c
rodriguezst <2828844+rodriguezst@users.noreply.github.com>
2024-10-15 12:54:30 +0000

    Linux 6.1.98

--- nabu-kernel_build.sh
+++ nabu-kernel_build.sh
@@ -1,11 +1,11 @@
-git clone https://github.com/map220v/sm8150-mainline.git --branch nabu-$1 --depth 1 linux
+git clone https://github.com/rodriguezst/linux.git --branch $1 --depth 1 linux
 cd linux
-make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- defconfig sm8150.config
+make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- xiaomi_nabu_defconfig
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
 _kernel_version="$(make kernelrelease -s)"
 mkdir ../linux-xiaomi-nabu/boot
 cp arch/arm64/boot/Image.gz ../linux-xiaomi-nabu/boot/vmlinuz-$_kernel_version
-cp arch/arm64/boot/dts/qcom/sm8150-xiaomi-nabu.dtb ../linux-xiaomi-nabu/boot/dtb-$_kernel_version
+cp arch/arm64/boot/dts/qcom/sm8150-xiaomi-nabu-maverick.dtb ../linux-xiaomi-nabu/boot/dtb-$_kernel_version
 sed -i "s/Version:.*/Version: ${_kernel_version}/" ../linux-xiaomi-nabu/DEBIAN/control
 rm -rf ../linux-xiaomi-nabu/lib
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=../linux-xiaomi-nabu modules_install
